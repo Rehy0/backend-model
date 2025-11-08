06f9c5f88eb485139c7d2df6d60006658ee0fdfe
Rehan Sheikh <rehansheikh0808@gmail.com>
2025-11-08 17:28:45 +0530

    Add account details update and user avatar upload functionalities with error handling

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -251,6 +251,50 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 const updateAccountDetails = asyncHandler(async(req,res)=>{
    const {fullname,email} = req.body;
    
+  if(!fullname || !email){
+    throw new ApiError(400,"All fields are required!")
+  }
+
+
+  const user = await User.findByIdAndUpdate(
+   req.user?.id,
+   {
+      $set:{
+         fullname:fullname, // fullname,
+         email:email  // email,
+      }
+   },
+   {new:true},
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user,"Account details update successfully!"))
+})
+
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+
+ const avatarLocalPath = req.file?.path
+
+if (!avatarLocalPath) {
+   throw new ApiError(400,"Avatar file is missing!");
+}
+
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+if (!avatar.url) {
+   throw new ApiError(400,"Error while uploading on avatar")
+}
+
+await User.findByIdAndUpdate(
+   req.user?._id,
+   {
+      $set:{
+         avatar:avatar.url
+      }
+   },
+   {new: true}
+)
 
 })
 
